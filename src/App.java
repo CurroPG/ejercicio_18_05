81731e7f77e73de09d46cfb4a8803d627660b03e
Curro Portillo <211curroportillo@gmail.com>
2025-10-28 16:57:10 +0100

    ejercicio 18

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero1 = 0;
+        int numero2 = 0;
+        do{
+            numero1 = Integer.parseInt(System.console().readLine("Introduzca un numero entero: "));
+            numero2 = Integer.parseInt(System.console().readLine("Introduzca otro numero entero distinto al anterior: "));
+            if (numero1 == numero2){
+                System.out.println("Los números introducidos no son válidos, deben ser distintos.");
+            }           
+        }while(numero1 == numero2);
+
+        int numeroMayor = Math.max(numero1, numero2);
+        int numeroMenor = Math.min(numero1, numero2);
+        
+        do{
+            System.out.print(numeroMenor + " ");
+            numeroMenor+=7;           
+        }while(numeroMenor<=numeroMayor);
     }
 }
